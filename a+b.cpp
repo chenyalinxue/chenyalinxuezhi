8a20d884a87b3d99907a7d9098a9c1865125565f
chenyalinxue <72291278+chenyalinxue@users.noreply.github.com>
2020-10-10 16:52:02 +0800

    Add files via upload

---
+++ a+b.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c;
+	cout<<"请分别输入两个数字a和b:";
+	cin >>a;
+	cin >>b;
+	c=a+b;
+	cout<<"这两个数字的和为："<<c;
+	return 0; 
+ } 
---
+++ a+b拓展.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b,c,d,e;
+	cout<<"请分别输入两个数字a和b:";
+	cin >>a;
+	cin >>b;
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	cout<<"这两个数字的和为："<<c<<'\n';
+	cout<<"这两个数字的差为："<<d<<'\n';
+	cout<<"这两个数字的积为："<<e<<'\n';
+	return 0; 
+ } 
